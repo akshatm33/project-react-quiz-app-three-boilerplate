a0d50a738e03f54612f8e6a084781ee37b2a2161
akshat33 <you@example.com>
2024-02-07 15:51:55 +0530

    update

---
+++ quiz-app-3/src/components/HomeComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../App.css"
+import { Link } from "react-router-dom";
+
+export default function HomeComponent(){
+
+  return(
+    <div className="main">
+      <h3 className="main-heading">Quiz App</h3>
+      <Link to="/play-quiz" className="mainButton">
+        <button className="mainButton">Play</button>
+      </Link>
+    </div>
+  )
+}
