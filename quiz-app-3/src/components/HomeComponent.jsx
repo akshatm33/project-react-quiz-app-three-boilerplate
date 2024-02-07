1059c1153428a373c6f00e2e1cf7e156d8608d36
akshat33 <you@example.com>
2024-02-07 16:27:23 +0530

    chnges

--- quiz-app-3/src/components/HomeComponent.jsx
+++ quiz-app-3/src/components/HomeComponent.jsx
@@ -7,7 +7,7 @@ export default function HomeComponent(){
   return(
     <div className="main">
       <h3 className="main-heading">Quiz App</h3>
-      <Link to="/play-quiz" className="mainButton">
+      <Link to="/play-quiz" className="mainButton1">
         <button className="mainButton">Play</button>
       </Link>
     </div>
